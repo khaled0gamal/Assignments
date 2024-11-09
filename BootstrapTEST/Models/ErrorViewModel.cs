56f1a6aa70e7eb23e47793ac3b0312199f5c5da8
khaled0gamal <ag8024360@gmail.com>
2024-11-09 10:47:28 +0300

    bootstrap

---
+++ BootstrapTEST/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootstrapTEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
